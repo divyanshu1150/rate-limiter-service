c4db58c4a13fb7e1af5d8331363cdb5d57409c2e
Divyanshu Agarwal <divyanshu1150@gmail.com>
2026-04-14 08:41:09 +0530

    Simplify RedisConfig: use application.properties for connection, only customize SSL peer verification

--- src/main/java/com/divyanshuagarwal/ratelimiter/config/RedisConfig.java
+++ src/main/java/com/divyanshuagarwal/ratelimiter/config/RedisConfig.java
@@ -1,43 +1,14 @@
 package com.divyanshuagarwal.ratelimiter.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-
-import java.net.URI;
 
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.data.redis.url:redis://localhost:6379}")
-    private String redisUrl;
-
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        URI uri = URI.create(redisUrl);
-        boolean useSsl = redisUrl.startsWith("rediss://");
-
-        String host = uri.getHost();
-        int port = uri.getPort() == -1 ? 6379 : uri.getPort();
-        String userInfo = uri.getUserInfo();
-        String password = userInfo != null && userInfo.contains(":") ? userInfo.split(":", 2)[1] : null;
-
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
-        if (password != null && !password.isEmpty()) {
-            config.setPassword(password);
-        }
-
-        LettuceClientConfiguration.LettuceClientConfigurationBuilder builder =
-                LettuceClientConfiguration.builder();
-
-        if (useSsl) {
-            builder.useSsl().disablePeerVerification();
-        }
-
-        return new LettuceConnectionFactory(config, builder.build());
+    public LettuceClientConfigurationBuilderCustomizer lettuceCustomizer() {
+        return builder -> builder.useSsl().disablePeerVerification();
     }
 }
