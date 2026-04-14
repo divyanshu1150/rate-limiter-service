4e199efddf58c51fdddd336c49fc1c6ddff6cab5
Divyanshu Agarwal <divyanshu1150@gmail.com>
2026-04-14 08:45:00 +0530

    Fix SSL: only disable peer verification when REDIS_SSL=true

--- src/main/java/com/divyanshuagarwal/ratelimiter/config/RedisConfig.java
+++ src/main/java/com/divyanshuagarwal/ratelimiter/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.divyanshuagarwal.ratelimiter.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,8 +8,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.ssl.enabled:false}")
+    private boolean sslEnabled;
+
     @Bean
     public LettuceClientConfigurationBuilderCustomizer lettuceCustomizer() {
-        return builder -> builder.useSsl().disablePeerVerification();
+        return builder -> {
+            if (sslEnabled) {
+                builder.useSsl().disablePeerVerification();
+            }
+        };
     }
 }
