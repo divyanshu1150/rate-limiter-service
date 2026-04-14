e680762ef04e63ef1b1a19ea563472c8ee0ea7d0
Divyanshu Agarwal <divyanshu1150@gmail.com>
2026-04-14 08:39:01 +0530

    Remove unused imports from RedisConfig

--- src/main/java/com/divyanshuagarwal/ratelimiter/config/RedisConfig.java
+++ src/main/java/com/divyanshuagarwal/ratelimiter/config/RedisConfig.java
@@ -1,7 +1,5 @@
 package com.divyanshuagarwal.ratelimiter.config;
 
-import io.lettuce.core.ClientOptions;
-import io.lettuce.core.SslOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
